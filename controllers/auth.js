1e7b3fcb3be253826b9e4f52e7d2ed264d7c94c6
Sergio Llanos <sergio.llanos.garcia@gmail.com>
2023-05-23 09:40:56 -0500

    Fin sección 11 - version 4.0.0

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const bcryptjs = require('bcryptjs');
 const Usuario = require('../models/usuario');
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 const authLogin = async (req, res) => {
 
@@ -44,6 +45,54 @@ const authLogin = async (req, res) => {
     }
 };
 
+const googleLogin = async (req, res, next) => {
+    const {id_token} = req.body;
+    console.log(id_token)
+
+    try {
+        const { nombre, picture, correo } = await googleVerify(id_token);
+
+        let usuario = await Usuario.findOne({ correo });
+        if(!usuario) {
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                picture,
+                google: true,
+                rol: "USER_ROL"
+            }
+
+            usuario = new Usuario(data);
+            usuario.save();
+        }
+
+        if(!usuario.estado) {
+            res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            })
+        }
+
+        const token = generarJWT(usuario.id);
+
+        res.json({
+            msg: 'Todo funciona correctamente',
+            usuario,
+            token
+        })
+
+    } catch(e) {
+        console.log(e)
+
+        res.status(400).json({
+            msg: 'El token no se pudo verificar'
+        })
+    }
+
+
+}
+
 module.exports = {
-    authLogin
+    authLogin,
+    googleLogin
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const { authLogin } = require("../controllers/auth");
+const { authLogin, googleLogin } = require("../controllers/auth");
 const { validarCampos } = require("../middlewares/validar-campos");
 const { check } = require("express-validator");
 
@@ -11,5 +11,10 @@ router.post('/login', [
     check('password', 'La contraseña es obligatoria').not().isEmpty(),
     validarCampos
 ], authLogin);
+
+router.post('/google', [
+    check('id_token', 'id_token es necesario').not().isEmpty(),
+    validarCampos
+], googleLogin);
 
 module.exports = router;
